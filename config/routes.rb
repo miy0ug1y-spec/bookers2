5673fb72094a62ce41722f60d30ec2511073b047
AyumiWatanabe <260193154+miy0ug1y-spec@users.noreply.github.com>
2026-05-01 11:17:30 +0000

    [Change]4-26

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  resource :session
+  resource :session,path: 'users', path_names: { new: 'sign_in' }
   resources :passwords, param: :token
   resources :users, path_names: { new: "sign_up" }
   resources :books
   
   root to:'homes#top'
-  get 'about', to: "homes#about", as: "about"
+  get 'home/about', to: "homes#about", as: "about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
